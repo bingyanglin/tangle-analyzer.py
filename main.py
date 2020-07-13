ee6d72975b983843da03f79e5e077ee67710d718
bingyanglin <bingyang.lin@iota.org>
2020-07-13 17:48:31 +0800

    Used assignment expression and added error handling in main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import iota
 import logging
 from pyfiglet import Figlet, figlet_format
-from tangleanalyzer import ZmqSub, AddressFilter, BundleFilter, TagFilter, TimeFilter, TransactionFilter, ValueFilter
 import toml
 from termcolor import cprint
-from tangleanalyzer import DmpDecode
+from tangleanalyzer import (
+    DmpDecode,
+    ZmqSub,
+    AddressFilter,
+    BundleFilter,
+    TagFilter,
+    TimeFilter,
+    TransactionFilter,
+    ValueFilter
+)
 
 
 def main():
@@ -16,59 +24,68 @@ def main():
     config = toml.load("config.toml")
 
     # Set the debug level
-    debug_level = config['logger']['level']
+    try:
+        debug_level = config['logger']['level']
+    except:
+        raise ValueError(
+            "Please provide the logger debuger level in the toml file!")
+
     logging.basicConfig(
         level=debug_level,
         format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
         datefmt="%H:%M:%S",
     )
-
     # Get the version
-    version = config['version']['name']
+    try:
+        version = config['version']['name']
+    except:
+        raise ValueError(
+            "Please provide the version name in the toml file!")
     logging.info(f"Version: {version}")
 
-    # Init the filters
-    filters = config.get("filters", {})
-    addr_set = set(filters.get('address', []))
-    bundle_set = set(filters.get('bundle', []))
-    tag_set = set(filters.get('tag', []))
-    transaction_set = set(filters.get('transaction', []))
-    time_list = filters.get('time', {})
-    value_list = filters.get('value', {})
-    zmq_conf = config.get("zmq", {})
-    dmp_conf = config.get("dmp", {})
+    # Get the filter configuration
+    filters_conf = config.get("filters", {})
 
     # Make filters
     filter_list = []
-    time_filter_list = []
-    dmp_time_filter_list = []
+    time_list = filters_conf.get('time', [])
 
-    if addr_set:
+    if addr_set := set(filters_conf.get('address', [])):
         filter_list.append(AddressFilter(addr_set).make_filter())
-    if bundle_set:
+    if bundle_set := set(filters_conf.get('bundle', [])):
         filter_list.append(BundleFilter(bundle_set).make_filter())
-    if tag_set:
+    if tag_set := set(filters_conf.get('tag', [])):
         filter_list.append(TagFilter(tag_set).make_filter())
-    if transaction_set:
+    if transaction_set := set(filters_conf.get('transaction', [])):
         filter_list.append(TransactionFilter(transaction_set).make_filter())
-    for t in value_list:
+    for v in filters_conf.get('value', []):
         filter_list.append(ValueFilter(
-            min=int(t['min']), max=int(t['max'])).make_filter(t['rlse']))
+            min=int(v['min']),
+            max=int(v['max'])).make_filter(v['rlse']))
 
-    if zmq_conf.get("enable", "FALSE") == "TRUE":
+    if (zmq_conf := config.get("zmq", {})).get("enable", "FALSE") == "TRUE":
+        time_filter_list = []
         for t in time_list:
             time_filter_list.append(TimeFilter(
-                start_date=t['start'], end_date=t['end']).make_filter(t['rlse']))
+                start_date=t['start'],
+                end_date=t['end']).make_filter(t['rlse']))
         sub = ZmqSub(url=zmq_conf['node_ip'],
-                     topic=zmq_conf['topic'], filterlist=filter_list+time_filter_list)
+                     topic=zmq_conf['topic'],
+                     filterlist=filter_list+time_filter_list)
         sub.run()
-    if dmp_conf.get("enable", "FALSE") == "TRUE":
+
+    if (dmp_conf := config.get("dmp", {})).get("enable", "FALSE") == "TRUE":
+        dmp_time_filter_list = []
         for t in time_list:
             dmp_time_filter_list.append(TimeFilter(
-                start_date=t['start'], end_date=t['end']).make_dmp_filter(t['rlse']))
-        dmpdecode = DmpDecode(dmp_folder=dmp_conf.get(
-            "input_folder", "dmp"), decoded_dmp_folder=dmp_conf.get(
-            "output_folder", "decoded_data"), filter_list=filter_list, time_filter_list=dmp_time_filter_list)
+                start_date=t['start'],
+                end_date=t['end']).make_dmp_filter(t['rlse']))
+
+        dmpdecode = DmpDecode(dmp_folder=dmp_conf.get("input_folder", "dmp"),
+                              decoded_dmp_folder=dmp_conf.get(
+                                  "output_folder", "decoded_data"),
+                              filter_list=filter_list,
+                              time_filter_list=dmp_time_filter_list)
         dmpdecode.run()
 
 
